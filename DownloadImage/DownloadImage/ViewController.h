3370a6115fa69454729c8d08ffb8c59598b51156
HarrisonWei <1512909687@qq.com>
2017-11-12 17:14:35 +0800

    初始化项目

---
+++ DownloadImage/DownloadImage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DownloadImage
+//
+//  Created by 曹魏 on 2017/11/12.
+//  Copyright © 2017年 itcast. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DownloadImage/DownloadImage/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DownloadImage
+//
+//  Created by 曹魏 on 2017/11/12.
+//  Copyright © 2017年 itcast. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
